150a0baea963a0556520529697c877ad70335bf6
theunistm <halltheunis@gmail.com>
2025-08-25 17:58:24 +0200

    implemented the navbar

--- portfolio-v5.1/astro.config.mjs
+++ portfolio-v5.1/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()],
   vite: {
     plugins: [tailwindcss()]
   }
